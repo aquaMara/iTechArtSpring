09ba7d24126da34c3eb79b5034dff91ecf569872
Mara <aquamarina.na@gmail.com>
2022-04-28 16:25:59 +0300

    fear(service): Add methods to return DTO

--- LearnRest/src/main/java/org/aquam/learnrest/service/ArticleService.java
+++ LearnRest/src/main/java/org/aquam/learnrest/service/ArticleService.java
@@ -10,11 +10,15 @@ import java.util.List;
 public interface ArticleService {
 
     Article findById(Long articleId);
+    ArticleDTO findByIdDTO(Long articleIdDTO);
     List<Article> findAll();
+    List<ArticleDTO> findAllDTO();
     Article create(ArticleDTO articleDTO, MultipartFile file) throws IOException;
     Article create(ArticleDTO articleDTO);
+    ArticleDTO createDTO(ArticleDTO articleDTO);
     Article addFile(Long articleId, MultipartFile file) throws IOException;
     Article updateById(Long articleId, ArticleDTO newArticle, MultipartFile file) throws IOException;
     Article updateById(Long articleId, ArticleDTO newArticleDTO);
+    ArticleDTO updateByIdDTO(Long articleId, ArticleDTO newArticleDTO);
     boolean deleteById(Long articleId);
 }
--- LearnRest/src/main/java/org/aquam/learnrest/service/SubjectService.java
+++ LearnRest/src/main/java/org/aquam/learnrest/service/SubjectService.java
@@ -10,12 +10,17 @@ import java.util.List;
 public interface SubjectService {
 
     Subject findById(Long subjectId);
+    SubjectDTO findByIdDTO(Long subjectId);
     Subject findByName(String name);
+    SubjectDTO findByNameDTO(String name);
     List<Subject> findAll();
+    List<SubjectDTO> findAllDTO();
     Subject create(SubjectDTO subjectDTO, MultipartFile file) throws IOException;
     Subject create(SubjectDTO subjectDTO);
+    SubjectDTO createDTO(SubjectDTO subjectDTO);
     Subject addFile(Long subjectId, MultipartFile file) throws IOException;
     Subject updateById(Long subjectId, SubjectDTO newSubjectDTO, MultipartFile file) throws IOException;
     Subject updateById(Long subjectId, SubjectDTO newSubjectDTO);
+    SubjectDTO updateByIdDTO(Long subjectId, SubjectDTO newSubjectDTO);
     boolean deleteById(Long subjectId);
 }
