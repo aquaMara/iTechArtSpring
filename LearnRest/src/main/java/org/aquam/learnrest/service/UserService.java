d23a8d669070b300f6d71e5d06f358f75445467e
Marina Tchernyshenko <81864572+aquaMara@users.noreply.github.com>
2022-04-29 00:41:58 +0300

    fear(service): Add methods to return DTO (#2)

--- LearnRest/src/main/java/org/aquam/learnrest/service/UserService.java
+++ LearnRest/src/main/java/org/aquam/learnrest/service/UserService.java
@@ -8,8 +8,11 @@ import java.util.List;
 public interface UserService {
 
     AppUser findById(Long id);
+    UserDTO findByIdDTO(Long id);
     List<AppUser> findAll();
+    List<UserDTO> findAllDTO();
     AppUser updateById(Long userId, UserDTO changedUserDTO);
+    UserDTO updateByIdDTO(Long userId, UserDTO changedUserDTO);
     boolean deleteById(Long userId);
     AppUser registerUser(UserDTO userDTO);
 
